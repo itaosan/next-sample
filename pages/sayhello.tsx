e006b09ffd0ddd0e878fbd25e1ad2adb8c87e3ab
itaosan <itaosan3@gmail.com>
2023-04-16 22:26:02 +0900

    p140

---
+++ pages/sayhello.tsx
@@ -0,0 +1,18 @@
+import { profile } from "console";
+import { useState,useEffect } from "react";
+
+function Sayhello() {
+    const [data,setData] = useState({name : ''})
+
+    useEffect(() => {
+        fetch('api/hello')
+        .then((res) => res.json())
+        .then((profile) => {
+            setData(profile)
+        })
+    },[])
+
+    return <div>hello {data.name}</div>
+}
+
+export default Sayhello
